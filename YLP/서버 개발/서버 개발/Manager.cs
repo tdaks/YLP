4b47610516a38cd5692a34122193101869f71507
Unknown <tdaks27@gmail.com>
2017-06-05 16:46:00 +0900

    개발할 서버 파일 푸쉬

---
+++ YLP/서버 개발/서버 개발/Manager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 서버_개발
+{
+    class Manager
+    {
+        static Manager singleton = null;
+        NetworkServer ns = null;
+
+        public static Manager Singleton
+        {
+            get
+            {
+                return Singleton;
+            }
+        }
+
+        private Manager() { }
+        static Manager()
+        {
+            singleton = new Manager();
+        }
+
+        public string MyIP
+        {
+            get
+            {
+                string host_name = Dns.GetHostName();
+                IPHostEntry host_entry = Dns.GetHostEntry(host_name);
+
+                foreach (IPAddress ipaddr in host_entry.AddressList)
+                {
+                    if (ipaddr.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        return ipaddr.ToString();
+                    }
+                }
+                return string.Empty;
+            }
+        }
+
+        public void StartCommunicateMobile()
+        {
+            //ns = new NetworkServer(MyIP, )
+        }
+    }
+}
---
+++ YLP/서버 개발/서버 개발/NetworkServer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 서버_개발
+{
+    class NetworkServer
+    {
+        TcpListener CommunicateMobileServer;
+
+        public NetworkServer(string ip, int port)
+        {
+
+
+        }
+    }
+}
